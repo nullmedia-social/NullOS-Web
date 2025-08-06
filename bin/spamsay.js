28c9fcf9b473888aad3eb3284920bb1e7415b743
KingNullboy <mjguydood@outlook.com>
2025-08-06 12:11:58 -0500

    Update spamsay.js

--- bin/spamsay.js
+++ bin/spamsay.js
@@ -1,10 +1,10 @@
 export default function(args, outputLine) {
   const message = args.join(' ') || 'HEY EVERY !! IT\'S ME!! SPAMTON G. SPAMTON!';
   outputLine(`${message}
-                  \
-                    \
-                      \
-                        \`);
+                  \\
+                    \\
+                      \\
+                        \\`);
   outputLine(`⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣤
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠁
 ⠀⠀⠀⠀⠀⠀⠀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠟⠛⠉⠀⠀⠀
